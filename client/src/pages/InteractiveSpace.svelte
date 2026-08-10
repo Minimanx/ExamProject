04352f5ec5d3810f0d2944a843485b17d94f3f9f
JS <johanschwennesen@gmail.com>
2026-08-10 15:03:43 +0200

    refactor: extract the street sign and logout icon

--- client/src/pages/InteractiveSpace.svelte
+++ client/src/pages/InteractiveSpace.svelte
@@ -11,6 +11,8 @@
   import { useLocation } from "svelte-navigator";
   import { Pulse } from "svelte-loading-spinners";
   import Skyline from "../art/Skyline.svelte";
+  import StreetSign from "../art/StreetSign.svelte";
+  import LogoutIcon from "../art/LogoutIcon.svelte";
 
   const socket = createSocket();
   const location = useLocation();
@@ -595,34 +597,7 @@
             </svg>
           {/if}
           {/each}
-          <svg
-            class="streetSign"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 -0.5 28 17"
-            shape-rendering="crispEdges"
-          >
-          <path
-            stroke="#163d1d"
-            d="M1 0h23M0 1h1M24 1h1M0 2h1M25 2h1M0 3h1M26 3h1M0 4h1M27 4h1M0 5h1M26 5h1M0 6h1M25 6h1M0 7h1M24 7h1M0 8h24"
-          />
-          <path
-            stroke="#25592e"
-            d="M1 1h2M5 1h19M1 2h1M3 2h22M1 3h1M3 3h2M6 3h2M9 3h1M11 3h1M13 3h2M17 3h2M21 3h5M1 4h1M4 4h1M6 4h5M12 4h3M16 4h1M18 4h1M20 4h1M22 4h5M1 5h1M3 5h2M6 5h2M9 5h2M12 5h3M16 5h1M18 5h1M21 5h5M1 6h1M3 6h3M7 6h1M9 6h1M11 6h1M13 6h2M17 6h2M20 6h1M22 6h3M1 7h23"
-          />
-          <path
-            stroke="#f7f5ed"
-            d="M3 1h2M2 2h1M2 3h1M5 3h1M8 3h1M10 3h1M12 3h1M15 3h2M19 3h2M2 4h2M5 4h1M11 4h1M15 4h1M17 4h1M19 4h1M21 4h1M2 5h1M5 5h1M8 5h1M11 5h1M15 5h1M17 5h1M19 5h2M2 6h1M6 6h1M8 6h1M10 6h1M12 6h1M15 6h2M19 6h1M21 6h1"
-          />
-          <path
-            stroke="#4d4d4d"
-            d="M4 9h1M19 9h1M4 10h1M19 10h1M4 11h1M19 11h1M4 12h1M19 12h1M4 13h1M19 13h1M4 14h1M19 14h1M4 15h1M19 15h1"
-          />
-          <path
-            stroke="#5c585c"
-            d="M5 9h1M20 9h1M5 10h1M20 10h1M5 11h1M20 11h1M5 12h1M20 12h1M5 13h1M20 13h1M5 14h1M20 14h1M5 15h1M20 15h1"
-          />
-          <path stroke="#a1855a" d="M18 15h1M2 16h5M17 16h5" />
-          </svg>
+          <StreetSign />
         </div>
         <div class="playerLayer">
           <div
@@ -677,17 +652,7 @@
         on:change={changeColor}
       />
       <button class="menuButton" id="logoutButton" on:click={logout}>
-        <svg
-          width="40px"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 -0.5 19 23"
-          shape-rendering="crispEdges"
-        >
-          <path
-            stroke="rgb(100, 100, 100)"
-            d="M0 0h15M0 1h15M0 2h6M13 2h2M0 3h2M4 3h4M13 3h2M0 4h2M6 4h4M13 4h2M0 5h2M8 5h2M13 5h2M0 6h2M8 6h2M13 6h2M0 7h2M8 7h2M13 7h2M0 8h2M8 8h2M13 8h2M0 9h2M8 9h2M16 9h2M0 10h2M8 10h2M11 10h8M0 11h2M8 11h2M11 11h8M0 12h2M8 12h2M16 12h2M0 13h2M8 13h2M13 13h2M0 14h2M8 14h2M13 14h2M0 15h2M8 15h2M13 15h2M0 16h2M8 16h2M13 16h2M0 17h2M8 17h2M13 17h2M0 18h2M8 18h7M0 19h4M8 19h7M2 20h4M8 20h2M4 21h6M6 22h4"
-          />
-        </svg>
+          <LogoutIcon />
       </button>
       <TheatersListView {theaters} {teleportToTheater} />
       {#if insideTheaterBool === true}
